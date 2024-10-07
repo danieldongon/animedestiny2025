1ff481af65e5a0bcc46b1d32f5dc992052128b54
Lionel Verano <lionel.verano.iv@berkeley.edu>
2024-10-06 19:57:29 -0700

    formatted artists.js. still missing loc

--- src/routes/artists/artists.js
+++ src/routes/artists/artists.js
@@ -2,26 +2,26 @@ export const artists = [
   {
     "name": "HuaXuan",
     "socials": "https://linktr.ee/huaxuan",
-    "description": "HuaXuan spends many of their waking hours (few as they might be) creating original illustrations, working on webcomics as an assistant, and attending conventions across the US. "
+    "description": "HuaXuan spends many of their waking hours (few as they might be) creating original illustrations, working on webcomics as an assistant, and attending conventions across the US."
   },
   {
     "name": "Ikaouji",
-    "socials": "IG @ Ikaouji",
-    "description": "Squid "
+    "socials": "https://www.instagram.com/Ikaouji",
+    "description": "Squid"
   },
   {
-    "name": "Beehumblehun ",
-    "socials": "IG: @bee.humble.hun",
-    "description": "Hello! My name is Bee and I make anime-style merchandise. You’ll find an array of prints, stickers, bookmarks, and charms at my table. I’m most commonly known for my Genshin Impact and Hazbin Hotel works but have many other fanart pieces to choose from. I look forward to this event! "
+    "name": "Beehumblehun",
+    "socials": "https://www.instagram.com/bee.humble.hun",
+    "description": "Hello! My name is Bee and I make anime-style merchandise. You’ll find an array of prints, stickers, bookmarks, and charms at my table. I’m most commonly known for my Genshin Impact and Hazbin Hotel works but have many other fanart pieces to choose from. I look forward to this event!"
   },
   {
     "name": "Cloverleafpie",
     "socials": "https://linktr.ee/cloverleafpie",
-    "description": "Hi friends!! I'm Jennifer, a Bay Area illustrator who makes a little bit of everything! Come find me for keychains, stickers, prints, apparel, and even plushies of cute little animals and yummy food~ "
+    "description": "Hi friends!! I'm Jennifer, a Bay Area illustrator who makes a little bit of everything! Come find me for keychains, stickers, prints, apparel, and even plushies of cute little animals and yummy food~"
   },
   {
     "name": "Rolilomi",
-    "socials": "rolilomi.carrd.co",
+    "socials": "https://rolilomi.carrd.co",
     "description": "your mom"
   },
   {
@@ -31,23 +31,23 @@ export const artists = [
   },
   {
     "name": "Midori Chan",
-    "socials": "Instagram.com/midori_chann",
-    "description": "Hi I am Midori and I am a local Bay Area artist. My art is focused primary on fandoms that I love with a mix of original art! You may find my art style to be cute! My smol biz sells stickers, art prints, and more. Catch me at Anime Destiny or my online shop! "
+    "socials": "https://www.instagram.com/midori_chann",
+    "description": "Hi I am Midori and I am a local Bay Area artist. My art is focused primarily on fandoms that I love with a mix of original art! You may find my art style to be cute! My smol biz sells stickers, art prints, and more. Catch me at Anime Destiny or my online shop!"
   },
   {
     "name": "momojiji",
-    "socials": "momojiji.carrd.co",
+    "socials": "https://momojiji.carrd.co",
     "description": "I'm the creator of webtoon series Swordid and Blackburn. I make comics, doujins, prints and merch to have original characters to hold or to pay tribute to their favorite anime. I also draw ORV. Come say hi!"
   },
   {
-    "name": "SneakyBunnies ",
-    "socials": "https://www.instagram.com/sneakybunniesxo?igsh=YzAwZjE1ZTI0Zg==",
-    "description": "SneakyBunnies celebrates Black and POC influence in the anime, comics, and gaming subculture. All with a cute kawaiicore style of her very own "
+    "name": "SneakyBunnies",
+    "socials": "https://www.instagram.com/sneakybunniesxo",
+    "description": "SneakyBunnies celebrates Black and POC influence in the anime, comics, and gaming subculture. All with a cute kawaiicore style of her very own."
   },
   {
     "name": "Animon Life",
-    "socials": "https://www.instagram.com/animonlife.official/",
-    "description": "Hello! We're Animon, a shop to cater all your mobile game merch needs ❤️ \n\nWe're one of the authorized distributor of all Hoyoverse game in the North American regions.\n\nAside from being a merchandise distributor, our goal is to recreate the atmosphere and vibe that you experience in the game to real life, which is what you'll experience if you visit our physical store. In addition, we are a little bit of everywhere locally to support others who share the same hobbies as us.\n\nView our previous showroom theme, and how we're invovled in the local community here: https://www.animonlifeofficial.com/physical-store\n"
+    "socials": "https://www.instagram.com/animonlife.official",
+    "description": "Hello! We're Animon, a shop to cater all your mobile game merch needs ❤️ We’re one of the authorized distributors of all Hoyoverse games in the North American regions. Our goal is to recreate the atmosphere and vibe that you experience in the game to real life, which is what you'll experience if you visit our physical store. We are also a little bit of everywhere locally to support others who share the same hobbies as us."
   },
   {
     "name": "CryPine",
@@ -60,64 +60,64 @@ export const artists = [
     "description": "Hello! I’m all about the nostalgic feels, especially when it comes to classic video games and anime. They’re my absolute favorite to make fanart of! Stop by my booth to check out stickers, keychains, pins, and art prints that bring those memories to life. <3"
   },
   {
-    "name": "Moonkeeart ",
+    "name": "Moonkeeart",
     "socials": "https://www.instagram.com/moonkee_art",
-    "description": "Small artist local to Stockton I mainly  do prints and sticker of fanart anything from anime to horror and all that in between. My art style is describe  as cute. "
+    "description": "Small artist local to Stockton I mainly do prints and stickers of fanart anything from anime to horror and all that in between. My art style is described as cute."
   },
   {
     "name": "Ash the Lazy",
-    "socials": "@ashthelazy on Instagram and Twitter",
+    "socials": "https://www.instagram.com/ashthelazy",
     "description": "Ash is a hobby artist who blends fanart and original creations, drawing inspiration from a variety of passions. Through her art, she creates unique merchandise that reflects her creative journey and celebrates her favorite series and themes."
   },
   {
     "name": "MicheleDraws",
-    "socials": "IG: michelecdraws",
+    "socials": "https://www.instagram.com/michelecdraws",
     "description": "I'm a Bay Area Illustrator that creates art to share part of my Filipino culture through mythology and astrology!"
   },
   {
     "name": "To The Sunnyside",
-    "socials": "https://tothesunnyside.carrd.co/",
+    "socials": "https://tothesunnyside.carrd.co",
     "description": "A poultry artist sharing their chicken scratches with the world."
   },
   {
     "name": "Neko-Oi",
-    "socials": "@justnekooi",
-    "description": "Bay area local artist focusing on cute cat art, food, and anime/games. "
+    "socials": "https://www.instagram.com/justnekooi",
+    "description": "Bay Area local artist focusing on cute cat art, food, and anime/games."
   },
   {
-    "name": "SquareBear Art ",
-    "socials": "@squarebearart on Instagram and Twitter ",
+    "name": "SquareBear Art",
+    "socials": "https://www.instagram.com/squarebearart",
     "description": "SquareBear Art is a mashup of kawaii aesthetic and graphic poster vibes. Taking inspiration from anime, videogames, pop culture and nostalgia you can find all kinds of silly, cute and aesthetic art... sometimes with a little bit of an edge!"
   },
   {
     "name": "Hissorihaka",
-    "socials": "IG and Twitter: hissorihaka ",
-    "description": "Hello, I’m Dee, and here I am trying to return not only to the AA world but in art in general after a much needed break :)"
+    "socials": "https://www.instagram.com/hissorihaka",
+    "description": "Hello, I’m Dee, and here I am trying to return not only to the AA world but in art in general after a much-needed break :)"
   },
   {
     "name": "doulbedo",
-    "socials": "twitter.com/doulbedo",
-    "description": "Hi, I'm Jun! I draw prints and make merch for my favorite series. Come stop by and talk about FF14/Alien Stage/Fire Emblem/Ace Attorney/Oblivion Battery with me! "
+    "socials": "https://twitter.com/doulbedo",
+    "description": "Hi, I'm Jun! I draw prints and make merch for my favorite series. Come stop by and talk about FF14/Alien Stage/Fire Emblem/Ace Attorney/Oblivion Battery with me!"
   },
   {
     "name": "pemprika",
-    "socials": "pemprika.carrd.co",
-    "description": "hi! i'm pemprika, a designer and artist who loves wistful and dreamy themes. ◠‿◠✿"
+    "socials": "https://pemprika.carrd.co",
+    "description": "hi! I'm pemprika, a designer and artist who loves wistful and dreamy themes. ◠‿◠✿"
   },
   {
     "name": "tokiitem",
-    "socials": "https://www.instagram.com/tokiitem?igsh=OGQ5ZDc2ODk2ZA%3D%3D&utm_source=qr",
-    "description": "Berkeley senior who occasionally sells art at local events!!!! Avid manga reader and enstars player 😙😙😙  Pls buy my goods 👯"
+    "socials": "https://www.instagram.com/tokiitem",
+    "description": "Berkeley senior who occasionally sells art at local events!!!! Avid manga reader and enstars player 😙😙😙 Pls buy my goods 👯"
   },
   {
     "name": "poxei",
-    "socials": "https://www.twitter.com/poxeii (if I can only put one link) ; https://www.instagram.com/poxei/ ; https://poxei.carrd.co/",
+    "socials": "https://www.twitter.com/poxeii",
     "description": "I like drawing my original characters and nostalgic fanart! Lately I've been obsessed with riso printing..."
   },
   {
     "name": "Rosemilk.T",
     "socials": "https://www.instagram.com/rosemilk.t",
-    "description": "Hello! I’m Stephanie, the artist behind Rosemilk.T. With a delicious cup of tea, I create cute artworks from my favorite animals, video games, and shows. Stop by to see my work and say hello! "
+    "description": "Hello! I’m Stephanie, the artist behind Rosemilk.T. With a delicious cup of tea, I create cute artworks from my favorite animals, video games, and shows. Stop by to see my work and say hello!"
   },
   {
     "name": "nenaco",
@@ -126,52 +126,52 @@ export const artists = [
   },
   {
     "name": "Starry Droplets",
-    "socials": "Instagram @ starrydroplets",
+    "socials": "https://www.instagram.com/starrydroplets",
     "description": "Hiya, I'm Sandy or Starry Droplets (〃ー〃) I'm an illustrator residing in the Central Valley of California. I am mostly a traditional watercolorist, but I like to dabble in other mediums such as digital painting, 3D modeling and printing. I vendor at different conventions and markets almost every month mainly across California, so maybe you'll see me at one in person! I hope to share and grow my art with all of you🌱"
   },
   {
     "name": "raineeski",
-    "socials": "https://www.instagram.com/raineeski/",
-    "description": "Hello hello! I’m rainee, and I love digital painting and sketching! Some of my favorite series include Houseki no Kuni, Hunter x Hunter, and Identity V (this one is a mobile game)!! So excited to see everyone in November! :>"
+    "socials": "https://www.instagram.com/raineeski",
+    "description": "Hello hello! I’m Rainee, and I love digital painting and sketching! Some of my favorite series include Houseki no Kuni, Hunter x Hunter, and Identity V!! So excited to see everyone in November!"
   },
   {
     "name": "Galaxsea Emporium",
-    "socials": "https://www.instagram.com/galaxseaemporium/ || https://www.etsy.com/shop/GalaxseaEmporium",
-    "description": "a pod of artists collecting and making anime and video game merch from the galaxy to the deep sea!"
+    "socials": "https://www.instagram.com/galaxseaemporium",
+    "description": "A pod of artists collecting and making anime and video game merch from the galaxy to the deep sea!"
   },
   {
     "name": "frogetful",
-    "socials": "_frogetful on Twitter",
-    "description": "Ask me about frogs! 𓆏 "
+    "socials": "https://twitter.com/_frogetful",
+    "description": "Ask me about frogs! 𓆏"
   },
   {
     "name": "AiTsuki Art",
-    "socials": "https://www.instagram.com/aitsukiart/",
+    "socials": "https://www.instagram.com/aitsukiart",
     "description": "I’m a local artist who draws anime chibis and some original art inspired by cute animals and food."
   },
   {
     "name": "lyflan",
-    "socials": "lyflan (Instagram)",
+    "socials": "https://www.instagram.com/lyflan",
     "description": "I make ducks, food art, and miscellaneous fanart, come say hi! :)"
   },
   {
     "name": "BlueCravat",
-    "socials": "songskye.com",
-    "description": "Hi!! I’m Sky and I make silly cats, random merch, food, and dramatic original art >:D\nEnjoyer of bitter green tea, mythology, and button-up up shirts with cool collars >:)\n"
+    "socials": "https://songskye.com",
+    "description": "Hi!! I’m Sky and I make silly cats, random merch, food, and dramatic original art >:D Enjoyer of bitter green tea, mythology, and button-up shirts with cool collars >:)"
   },
   {
     "name": "fxt soba",
-    "socials": "Instagram: @fxtsoba, Linktree: linktr.ee/fxtsoba",
+    "socials": "https://linktr.ee/fxtsoba",
     "description": "Hi, I'm Cy, an AAPI and queer artist based in the San Francisco Bay Area (and Cal Alumnus). I create stickers, bags, keychains, prints, mousepads, air fresheners, standees, microfiber cloths, and so much more! I love making unique specialty items."
   },
   {
     "name": "purinspace",
     "socials": "https://www.instagram.com/purinspace",
-    "description": "i like to crochet lil creatures and draw things i like whether it be from video games or anime :3"
+    "description": "I like to crochet lil creatures and draw things I like whether it be from video games or anime :3"
   },
   {
     "name": "chowpan",
-    "socials": "instagram.com/chowpandraws",
-    "description": "I'm chow! A former CAA officer who loves seinen/shounen. Currently into Kaiju no. 8, Blue Lock, Windbreaker, Dungeon Meshi, and cats! yippeee"
+    "socials": "https://www.instagram.com/chowpandraws",
+    "description": "I'm Chow! A former CAA officer who loves seinen/shounen. Currently into Kaiju no. 8, Blue Lock, Windbreaker, Dungeon Meshi, and cats! Yippeee"
   }
 ];

bf8020145e308bd8ea7ae10ebd8e4c70a1ce9b93
neptunia <kevinshen100@gmail.com>
2024-10-09 16:31:50 -0700

    about us page done i guess

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,15 +11,19 @@
     </div>
     <div class="page-body">
 
-      <p>Anime Destiny is Berkeley's annual anime convention hosted by Cal Animage Alpha, a non-profit student organization founded at UC Berkeley and thriving since 1989. </p>
+      <p><b>Anime Destiny</b> is Berkeley's annual anime convention hosted by <b>Cal Animage Alpha</b>, a non-profit student organization founded at UC Berkeley and thriving since 1989. </p>
 
-      <p>Cal Animage Alpha is the first anime club of the Cal Animage network, a group of anime clubs that has expanded to universities across the country. Focused on all things anime, manga, and games, our club founders have gone on to create Crunchyroll and Anime Expo, among many other amazing anime-related events and organizations. Here at Cal Animage Alpha, we aim to foster a supportive and welcoming environment where all types of people can gather and meet like-minded individuals. </p>
+      <p><b>Cal Animage Alpha</b> is the first anime club of the Cal Animage network, a group of anime clubs that has expanded to universities across the country. Focused on all things anime, manga, and games, our club founders have gone on to create Crunchyroll and Anime Expo, among many other amazing anime-related events and organizations. Here at Cal Animage Alpha, we aim to foster a supportive and welcoming environment where all types of people can gather and meet like-minded individuals. </p>
         
-      <p>To this end, we created Anime Destiny. At this annual one-day convention, we invite artists, panelists, guests, speakers, performers, industry professionals, and many more types of people involved in the anime industry to come together and create an amazing experience for the public. Anime Destiny is open to everyone! Especially those who enjoy all things “animage” (anime, manga, and games) or want to learn more about it. </p>
+      <p>To this end, we created <b>Anime Destiny</b>. At this annual one-day convention, we invite artists, panelists, guests, speakers, performers, industry professionals, and many more types of people involved in the anime industry to come together and create an amazing experience for the public. Anime Destiny is open to everyone! Especially those who enjoy all things “animage” (anime, manga, and games) or want to learn more about it. </p>
       <p>  
         We hope you have an amazing time at Anime Destiny this year and thank you for supporting Cal Animage Alpha!
-        </p>
-      <img src="debug/yellow.png" alt="Work in Progress" style="width: 100px; height: auto;" />
+      </p>
+
+      <a href="https://cal.moe/about" class="section-link">Learn more about Cal Animage Alpha</a>
+      <br/><br/>
+
+      <img src="images/animage.jpg" alt="Work in Progress" style="width: 80%; height: auto;margin:auto;" />
 
     </div>
     
@@ -40,7 +44,7 @@
       display: flex; /* Flexbox for centering */
       justify-content: center; /* Horizontally center the text */
       align-items: center; /* Vertically center the text */
-      width: 100vw; /* Full viewport width */
+      width: 100%; /* Full viewport width */
       margin: 0; /* Remove any default margin */
       box-sizing: border-box; /* Ensures padding doesn't affect width */
     }
@@ -53,6 +57,24 @@
     }
     p {
       margin-bottom: 1rem;
+    }
+
+    .section-link {
+      color: #485077;
+      text-decoration: none;
+      font-weight: bold;
+      font-size: 1.1rem;
+      border-bottom: 2px solid #485077; /* Underline links */
+      padding-bottom: 0.2rem;
+      line-height:4rem;
+      padding-top: 2rem;
+      transition: color 0.3s ease, border-bottom-color 0.3s ease;
+    }
+
+    .section-link:hover {
+      color: #fed893; /* Yellow on hover */
+      border-bottom-color: #fed893;
+      
     }
   </style>
   

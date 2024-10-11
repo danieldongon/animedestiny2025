e70bea981118f0c1d00c4db72b70b3c906838aea
Lionel Verano <lionel.verano.iv@berkeley.edu>
2024-10-11 13:52:16 -0700

    draft schedule and updated event info

---
+++ src/lib/components/Calendar.svelte
@@ -0,0 +1,11 @@
+<script>
+    let calendar = "https://calendar.google.com/calendar/embed?src=your_google_calendar_id&ctz=America/Los_Angeles";
+  </script>
+  
+  <div style="width: 100%; height: 600px; margin-top: 1rem;">
+    <iframe 
+      src= {calendar} 
+      style="border: 0" width="100%" height="100%" frameborder="0" scrolling="no"
+      title="Google Calendar">
+    </iframe>
+  </div>

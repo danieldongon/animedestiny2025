8e49250e81eab4ef4b0d3306fafb320a59be9620
neptunia <kevinshen100@gmail.com>
2024-10-29 20:51:28 -0700

    some SEO/embed info

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -8,5 +8,9 @@
   <link rel="icon" href="icons/ADicon.png"/>
 
   <!-- Set meta tags for SEO purposes -->
-  <meta name="description" content="Welcome to Anime Destiny 2024 hosted by Cal Animage Alpha! Tickets are now live; don't wait and join in on the exciting events, exhibits, and games at UC Berkeley's premiere anime convention." />
+  <link type="application/json+oembed" href="metadata.json" />
+  <meta content='images/ADLogo.png' property='og:image'>
+  <meta name="twitter:card" content="summary_large_image">
+  <meta name="theme-color" content="#fed893">
+  <meta name="description" content="Welcome to Anime Destiny 2024 hosted by Cal Animage Alpha! Tickets are now live; don't wait and join in on the exciting events, exhibits, and games at UC Berkeley's premiere anime convention. Happening on Saturday, November 9th from 9:00 AM to 7:00 PM." />
 </svelte:head>

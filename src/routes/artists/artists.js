615174a4dcd54c86d9c1ea97449c36b6ad94e9fa
neptunia <kevinshen100@gmail.com>
2024-10-29 20:35:33 -0700

    im a clown and forgot a comma

--- src/routes/artists/artists.js
+++ src/routes/artists/artists.js
@@ -70,7 +70,7 @@ export const artists = [
     "socials": "https://bart.gov/anime",
     "description": "The San Francisco Bay Area Rapid Transit District (BART) is a heavy-rail public transit system that connects the San Francisco Peninsula with communities in the East Bay and South Bay. BART is excited to announce the release of five BART- and Bay Area-themed anime mascots that will help us in our mission to promote public transportation use, especially among youth riders, a growing and reliable ridership demographic.",
     "location": "AB1"
-  }
+  },
   {
     "name": "Precious Touch Creations",
     "socials": "https://www.precioustouchcreations.com/",

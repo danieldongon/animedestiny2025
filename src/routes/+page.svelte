7d9751832a6773baa10a693ff5b54e7796e0c3d3
Lionel Verano <lionel.verano.iv@berkeley.edu>
2024-11-05 16:46:42 -0800

    Revert "replaced calendar with FullCalendar component"
    This reverts commit 5dcfb0881fe5ad45ea818248a7b4f3545ad6fd1b.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,11 +2,10 @@
   import { onMount } from 'svelte';
   import Navbar from '$lib/components/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
-  import Calendar from '$lib/components/Calender.svelte';
+  // import Calendar from '$lib/components/Calendar.svelte';
   import Gaming from '$lib/components/Gaming.svelte';
   import Events from '$lib/components/Events.svelte';
   import { fade } from 'svelte/transition';
-  import Calender from '$lib/components/Calender.svelte';
 
   let desktopImage = 'images/hero.png';
   let mobileImage = 'images/hero.png';
@@ -260,7 +259,7 @@
         
         
 
-        <!-- <div class="content-container">
+        <div class="content-container">
           <img src="images/schedule.png" class="ADMap" alt="Artist Alley Map" on:click={openOverlayADSchedule} />
           <div style="padding:1rem 0;">
           <a on:click={openOverlayADSchedule}>Click for larger image!</a>
@@ -271,8 +270,7 @@
             <button class="close-button-artist-map" on:click={closeOverlayADSchedule}>✕</button>
             <img src="images/schedule.png" on:click|stopPropagation class="image-artist-map" alt="Fullscreen AD Map" />
           </div>
-        {/if} -->
-        <Calender/>
+        {/if}
         
         <div class="content-container">
           <img src="images/AD map.png" class="ADMap" alt="Artist Alley Map" on:click={openOverlayADMap} />

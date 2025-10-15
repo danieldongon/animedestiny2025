254d84670ad594fbcf19876331974da29240d141
Sasha <lopez.sasha2003@gmail.com>
2025-10-15 13:17:02 -0700

    Fixed broken links.js
    Had to restore links to a single string line, if someone knows how to make them into hyperlinks that have a line of text and can link into url that'd be much appreciated.

--- src/routes/artists/artists.js
+++ src/routes/artists/artists.js
@@ -1,192 +1,103 @@
 export const artists = [
     {
     "name": "chthonicArchivist ",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://chthonicArchivist.carrd.co"
-      }
-    ],
+    "socials": "https://chthonicArchivist.carrd.co",
     "description": "",
     "location": "A1"
   },
   {
     "name": "driedmangoenjoyer",
-    "socials": [
-      {
-        "text":"Instgram",
-        "url":"https://www.instagram.com/driedmangoenjoyer"
-      }
-    ],
+    "socials": "https://www.instagram.com/driedmangoenjoyer",
     "description": "",
     "location": "A1"
   },
   {
     "name": "tokiitem",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://instagram.com/tokiitem"
-      }
-    ],
+    "socials": "https://instagram.com/tokiitem",
     "description": "",
     "location": "A2"
   },
   {
     "name": "mousec1ub",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/mousec1ub"
-      }
-    ],
+    "socials": "https://www.instagram.com/mousec1ub",
     "description": "",
     "location": "A3"
   },
   {
     "name": "Butterchu",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/butterchuxdrawings/"
-      }, 
-      {
-        "text":"Twitter",
-        "url":"https://x.com/Butterchuxdraw"
-      }
-  ],
+    "socials": "https://www.instagram.com/butterchuxdrawings/",
     "description": "",
     "location": "A4"
   },
   {
     "name": "Fabucasle",
-    "socials": [
-      {
-        "text":"Twitter",
-        "url":"https://twitter.com/fabucastle"
-      }
-    ],
+    "socials": "https://twitter.com/fabucastle",
     "description": "",
     "location": "A5-A6"
   },
   {
     "name": "Ikaouji",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://Ikaouji.bigcartel.com"
-      }
-    ],
+    "socials": "https://Ikaouji.bigcartel.com",
     "description": "",
     "location": "A7"
   },
   {
     "name": "MisulMint",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://misulstudios.wixsite.com/misulstudios/links"
-      }
-    ],
+    "socials": "https://misulstudios.wixsite.com/misulstudios/links",
     "description": "",
     "location": "A8"
   },
   {
     "name": "Art of Mazzy",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"http://artofmazzy.com/"
-      }
-    ],
+    "socials": "http://artofmazzy.com/",
     "description": "",
     "location": "A9"
   },
   {
     "name": "Mewchao",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://mewchao.com"
-      }
-    ],
+    "socials": "https://mewchao.com",
     "description": "",
     "location": "B1"
   },
   {
     "name": "Saiyakyy",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://saiyakyy.carrd.co/"
-      }
-    ],
+    "socials": "https://saiyakyy.carrd.co/",
     "description": "",
     "location": "B2"
   },
   {
     "name": "purinspace",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/purinspace"
-      }
-    ],
+    "socials": "https://www.instagram.com/purinspace",
     "description": "",
     "location": "B2"
   },
   {
     "name": "remirii",
-    "socials": [
-      {
-        "text":"Twitter",
-        "url":"https://x.com/remiriin"
-      }
-    ],
+    "socials": "https://x.com/remiriin",
     "description": "",
     "location": "B3"
   },
   {
     "name": "kahel",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/c_ffeejelly/"
-      }
-    ],
+    "socials": "https://www.instagram.com/c_ffeejelly/",
     "description": "",
     "location": "B4"
   },
   {
     "name": "Draconli",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://www.kirstenandersenartist.com"
-      }
-    ],
+    "socials": "https://www.kirstenandersenartist.com",
     "description": "",
     "location": "B5"
   },
   {
     "name": "Morning Tea",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/my_morningtea/"
-      }
-    ],
+    "socials": "https://www.instagram.com/my_morningtea/",
     "description": "",
     "location": "B6"
   },
   {
     "name": "Glacia",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://glaciavt.carrd.co"
-      }
-    ],
+    "socials": "https://glaciavt.carrd.co",
     "description": "",
     "location": "B7"
   },
@@ -198,23 +109,13 @@ export const artists = [
   },
   {
     "name": "Komun Horangi",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/komunhorangi/"
-      }
-    ],
+    "socials": "https://www.instagram.com/komunhorangi/",
     "description": "",
     "location": "B9"
   },
   {
     "name": "LilyCli",
-        "socials": [ 
-          {
-            "text":"Webpage",
-            "url":"https://lihttps://lilycli.com"
-          }
-        ],
+        "socials": "https://lihttps://lilycli.com",
     "description": "",
     "location": "C1"
   },
@@ -226,188 +127,103 @@ export const artists = [
   },
   {
     "name": "LenisLynx",
-    "socials": [
-      {
-        "text":"Tumblr",
-        "url":"https:lenislynx.tumblr.com"
-      }
-    ],
+    "socials": "https:lenislynx.tumblr.com",
     "description": "",
     "location": "C2"
   },
   {
     "name": "Studio Sushisox",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/studiosushisox/"
-      }
-    ],
+    "socials": "https://www.instagram.com/studiosushisox/",
     "description": "",
     "location": "C3"
   },
   {
     "name": "SonoNabe",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/sono.nabe?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr"
-      }
-    ],
+    "socials": "https://www.instagram.com/sono.nabe?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr",
     "description": "",
     "location": "C3"
   },
   {
     "name": "Galaxsea Emporium",
-    "socials": [
-      {
-        "text":"Etsy",
-        "url":"https://Galaxseaemporium.etsy.com"
-      }
-    ],
+    "socials": "https://Galaxseaemporium.etsy.com",
     "description": "",
     "location": "C4"
   },
   {
     "name": "Yuerise",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/yuerise"
-      }
-    ],
+    "socials": "https://www.instagram.com/yuerise",
     "description": "",
     "location": "C5"
   },
   {
     "name": "Hideawaymelon",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://hideawaymelon.com/"
-      }
-    ],
+    "socials": "https://hideawaymelon.com/",
     "description": "",
     "location": "C6"
   },
   {
     "name": "Lem's World",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/lemsworld_"
-      }
-    ],
+    "socials": "https://www.instagram.com/lemsworld_",
     "description": "",
     "location": "C7"
   },
   {
     "name": "AlcorChannel",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://alcorchannel.com/"
-      }
-    ],
+    "socials": "https://alcorchannel.com/",
     "description": "",
     "location": "C8"
   },
   {
     "name": "Redteapanda",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://redteapanda.carrd.co/"
-      }
-    ],
+    "socials": "https://redteapanda.carrd.co/",
     "description": "",
     "location": "C9"
   },
   {
     "name": "p1ku00",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/p1ku00?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr"
-      }
-    ],
+    "socials": "https://www.instagram.com/p1ku00?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr",
     "description": "",
     "location": "D1"
   },
   {
     "name": "Jesdee",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/jesvded/"
-      }
-    ],
+    "socials": "https://www.instagram.com/jesvded/",
     "description": "",
     "location": "D1"
   },
   {
     "name": "Aretevir",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://artstation.com/crha"
-      }
-    ],
+    "socials": "https://artstation.com/crha",
     "description": "",
     "location": "D2"
   },
   {
     "name": "kwnce",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/kwnce/"
-      }
-    ],
+    "socials": "https://www.instagram.com/kwnce/",
     "description": "",
     "location": "D2"
   },
   {
     "name": "Ratfestival",
-    "socials": [
-      {
-        "text":"GitHub",
-        "url":"https://ratfestival.github.io"
-      }
-    ],
+    "socials": "https://ratfestival.github.io",
     "description": "",
     "location": "D3"
   },
   {
     "name": "Batsouppe",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://batsouppe.art"
-      }
-    ],
+    "socials": "https://batsouppe.art",
     "description": "",
     "location": "D4"
   },
   {
     "name": "JZhangDraws",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/jzhangdraws/"
-      }
-    ],
+    "socials": "https://www.instagram.com/jzhangdraws/",
     "description": "",
     "location": "D5"
   },
   {
     "name": "Doulbedo",
-    "socials": [
-      {
-        "text":"Twitter",
-        "url":"https://x.com/doulbedo?lang=en"
-      }
-    ],
+    "socials": "https://x.com/doulbedo?lang=en",
     "description": "",
     "location": "D6"
   },
@@ -419,122 +235,67 @@ export const artists = [
   },
   {
     "name": "Dog Ear (tanuweir)",
-    "socials": [
-      {
-        "text":"Twitter",
-        "url":"https://x.com/tanuweir"
-      }
-    ],
+    "socials": "https://x.com/tanuweir",
     "description": "",
     "location": "D7"
   },
   {
     "name": "wen",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/wen_draws/"
-      }
-    ],
+    "socials": "https://www.instagram.com/wen_draws/",
     "description": "",
     "location": "D8"
   },
   {
     "name": "Bunibelles",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/bunibelles?igsh=NTc4MTIwNjQ2YQ=="
-      }
-    ],
+    "socials": "https://www.instagram.com/bunibelles?igsh=NTc4MTIwNjQ2YQ==",
     "description": "",
     "location": "D8"
   },
   {
     "name": "LYFLAN",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://instagram.com/lyflan"
-      }
-    ],
+    "socials": "https://instagram.com/lyflan",
     "description": "",
     "location": "D9"
   },
   {
     "name": "izzyxue_",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/izzyxue_?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr"
-      }
-    ],
+    "socials": "https://www.instagram.com/izzyxue_?igsh=NTc4MTIwNjQ2YQ%3D%3D&utm_source=qr",
     "description": "",
     "location": "D9"
   },
   {
     "name": "AiTsuki Art",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/aitsukiart/"
-      }
-    ],
+    "socials": "https://www.instagram.com/aitsukiart/",
     "description": "",
     "location": "E1-E2"
   },
   {
     "name": "Suki Suki Merch",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/sukisukimerch/"
-      }
-    ],
+    "socials": "https://www.instagram.com/sukisukimerch/",
     "description": "",
     "location": "E3"
   },
   {
     "name": "eckoeckoss studio",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://www.eckoeckos.com/welcome/"
-      }
-    ],
+    "socials": "https://www.eckoeckos.com/welcome/",
     "description": "",
     "location": "E4"
   },
   {
     "name": "Casually Cosplay",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://Casuallycosplay.com"
-      }
-    ],
+    "socials": "https://Casuallycosplay.com",
     "description": "",
     "location": "E5"
   },
   {
     "name": "soyemilky",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://soyemilky.carrd.co"
-      }
-    ],
+    "socials": "https://soyemilky.carrd.co",
     "description": "",
     "location": "E6"
   },
   {
     "name": "Pachito",
-    "socials": [
-      {
-        "text": "Instagram",
-        "url": "https://www.instagram.com/bachipott?igsh=MzRlODBiNWFlZA=="
-      }
-    ],
+    "socials": "https://www.instagram.com/bachipott?igsh=MzRlODBiNWFlZA==",
     "description": "",
     "location": "",
   },
@@ -546,45 +307,25 @@ export const artists = [
   },
   {
     "name": "NyamenRamen",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://nyamenramen.carrd.co/"
-      }
-    ],
+    "socials": "https://nyamenramen.carrd.co/",
     "description": "",
     "location": "E8"
   },
   {
     "name": "Joziar",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/joziarl/"
-      }
-    ],
+    "socials": "https://www.instagram.com/joziarl/",
     "description": "",
     "location": "E8"
   },
   {
     "name": "Shigezaki",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://shigezaki.carrd.co/"
-      }
-    ],
+    "socials": "https://shigezaki.carrd.co/",
     "description": "",
     "location": "E9"
   },
   {
     "name": "BONE",
-    "socials": [
-      {
-        "text":"Twitter",
-        "url":"https://x.com/BONEEEeeZzz"
-      }
-    ],
+    "socials": "https://x.com/BONEEEeeZzz",
     "description": "",
     "location": "E9"
   },
@@ -596,152 +337,80 @@ export const artists = [
   },
   {
     "name": "yumitang_",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://www.yumitang.art/"
-      }
-    ],
+    "socials": "https://www.yumitang.art/",
     "description": "",
     "location": "F3-F4"
   },
   {
     "name": "maplesyrupbean",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://maplesyrupbean.carrd.co/"
-      }
-    ],
+    "socials": "https://maplesyrupbean.carrd.co/",
     "description": "",
     "location": "F5"
   },
   {
     "name": "Mintybytes",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://mintybytes.com"
-      }
-    ],
+    "socials": "https://mintybytes.com",
     "description": "",
     "location": "F6"
   },
   {
     "name": "Spirafall",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://spirafall.net/"
-      }
-    ],
+    "socials": "https://spirafall.net/",
     "description": "",
     "location": "F7"
   },
   {
   "name": "JWijaya Art",
-  "socials": [
-    {
-      "text":"Webpage",
-      "url":"http://www.kailunqu.com"
-    }
-  ],
+  "socials": "http://www.kailunqu.com",
   "description": "",
   "location": "F8"
   },
   {
       "name": "Oolongcreama & AmaiCandy",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://ideloreart.com/#merch"
-      },
-      {
-        "text":"Twitter",
-        "url":"https://x.com/_amaicandy"
-      }
-    ],
+    "socials": "https://ideloreart.com/#merch",
     "description": "",
     "location": "F9"
   },
   {
     "name": "_naidraws",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://Www.instagram.com/_naidraws"
-      }
-    ],
+    "socials": "https://Www.instagram.com/_naidraws",
     "description": "",
     "location": "G1"
   },
   {
     "name": "Ash the Lazy",
-    "socials": [
-      {
-        "text":"Myportfolio",
-        "url":"https://ashthelazy.myportfolio.com"
-      }
-    ],
+    "socials": "https://ashthelazy.myportfolio.com",
     "description": "",
     "location": "G2"
   },
   {
     "name": "mlkinis",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://mlkinis.carrd.co/"
-      }
-    ],
+    "socials": "https://mlkinis.carrd.co/",
     "description": "",
     "location": "G3"
   },
   {
     "name": "poxei",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://poxei.carrd.co"
-      }
-    ],
+    "socials": "https://poxei.carrd.co",
     "description": "",
     "location": "G4"
   },
   {
     "name": "Tanisquirrel",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://tanisquirrel.carrd.co/"
-      }
-    ],
+    "socials": "https://tanisquirrel.carrd.co/",
     "description": "",
     "location": "G5"
   },
   {
     "name": "Effervessa",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://https://www.effervessa.com/"
-      },
-      {
-        "text":"Instagram",
-        "url":"https://instagram.com/raineeski/"
-      }
-              ],
+    "socials": 
+        "https://https://www.effervessa.com/",
     "description": "",
     "location": "G6"
   },
   {
     "name": "Imperial Knight",
-    "socials": [
-      {
-        "text":"Linktree",
-        "url":"https://linktr.ee/imperialknight"
-      }
-    ],
+    "socials": "https://linktr.ee/imperialknight",
     "description": "",
     "location": "G7"
   },
@@ -753,34 +422,19 @@ export const artists = [
   },
   {
     "name": "CHIOU!",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com"
-      }
-    ],
+    "socials": "https://www.instagram.com",
     "description": "",
     "location": "G8"
   },
   {
     "name": "lixizu",
-    "socials": [
-      {
-        "text":"Webpage",
-        "url":"https://lixizu.shop"
-      }
-    ],
+    "socials": "https://lixizu.shop",
     "description": "",
     "location": "G9"
   },
   {
     "name": "Genshin at Berkeley",
-    "socials": [
-      {
-        "text":"Lintree",
-        "url":"https://linktr.ee/genshinatberkeley"
-      }
-    ],
+    "socials": "https://linktr.ee/genshinatberkeley",
     "description": "",
     "location": "H1"
   },
@@ -792,12 +446,7 @@ export const artists = [
   },
   {
     "name": "The Trinket Hall",
-    "socials": [
-      {
-        "text":"Instagram",
-        "url":"https://www.instagram.com/trinket.hall/"
-      }
-    ],
+    "socials": "https://www.instagram.com/trinket.hall/", 
     "description": "",
     "location": "H4-H6"
   },

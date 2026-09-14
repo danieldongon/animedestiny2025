894088b2edbcf73b92af863cc44ac62a87a9d5e7
Fakintosh <bli31416@gmail.com>
2026-09-13 19:55:07 -0700

    updated special guests

--- src/routes/special-guests/guests.js
+++ src/routes/special-guests/guests.js
@@ -12,5 +12,26 @@ export const guests = [
     "social_type":"instagram",
     "image":"/images/guests/Ray Chase.jpeg",
     "blurb":"Meet Ray Chase, known in the voice acting industry for his contributions to numerous different medias as roles including: Neuvillette in Genshin Impact; Noctis Lucis Caelum in Final Fantasy XV; Sukuna and Choso in Jujutsu Kaisen; Tengen Uzui in Demon Slayer; Bruno Bucciarati in JoJo’s Bizarre Adventure; Fyodor Dostoevsky in Bungo Stray Dogs; Cyclops in X-Men ‘97; Silent Salt Cookie in Cookie Run Kingdom; and more. \n\n\nAutograph times: 11 AM - 12:30 PM and 2:30 PM - 6 PM in Pauley Ballroom!"
+  },
+  {
+    "name":"Tytalis",
+    "socials":"https://www.instagram.com/tytalis",
+    "social_type":"instagram",
+    "image":"/images/guests/Tytalis Photo.jpeg",
+    "blurb":"Tytalis Cosplay has been cosplaying since 2012, and has won several awards for her work and performances from cons like Anime Los Angeles and San Diego Comic Con. Her love of cosplay inspired her to pursue engineering, and she received her Master of Engineering from UC Berkeley in 2019! She is self taught, and believes that cosplay is for everyone, and wants to inspire others to follow their passions."
+  },
+  {
+    "name":"Cesseciel",
+    "socials":"https://www.instagram.com/cesseciel",
+    "social_type":"instagram",
+    "image":"/images/guests/Cesseciel Photo.png",
+    "blurb":"Cesseciel (He/They), also known as Ciel, is a cosplayer, performer, and seamstress based in the Bay Area. He has been sewing since the age of four and has been crafting his own cosplays since he first began attending conventions in 2013. Ciel can often be found onstage performing idol dance covers or with his fellow maids at the Ai Hoshi Cafe!"
+  },
+  {
+    "name":"Selene (miaucosplay)",
+    "socials":"https://www.instagram.com/miaucosplay",
+    "social_type":"instagram",
+    "image":"/images/guests/Miaucosplay Photo.jpeg",
+    "blurb":"meow"
   }
 ];

52c646b0c70bf6eb47c171e5dd78e5a4d4b75d5f
Fakintosh <bli31416@gmail.com>
2026-09-23 21:10:29 -0700

    added twitter/x thing for special guests

--- src/routes/special-guests/+page.svelte
+++ src/routes/special-guests/+page.svelte
@@ -104,7 +104,7 @@
                   </svg>
                 </a>
                 {/if}
-                {#if guest.social_type === 'youtube'}
+                {#if guest.social_type === "youtube"}
                 <a
                   class="inline-block mt-3 text-red-500 hover:text-red-600"
                   href={guest.socials}
@@ -114,6 +114,19 @@
                 >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-10 h-10" viewBox="0 0 16 15.5">
                   <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
+                </svg>
+                </a>
+                {/if}
+                {#if guest.social_type === "twitter"}
+                <a
+                  class="inline-block mt-3 text-black-500 hover:text-black-600"
+                  href={guest.socials}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Follow on Twitter/X"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-10 h-10" viewBox="0 0 16 15.5">
+                  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                 </svg>
                 </a>
                 {/if}
@@ -133,6 +146,7 @@
 <div class="container mx-auto  px-4 py-4">
     <p>We're also excited to announce our special Vtuber guest lineup for Anime Destiny 2026! Click on each image to learn about them.</p>
   </div>
+<!--note to self: pls make a function so the code isn't repeated twice :p-->
 <div  class="container mx-auto px-4 py-12"></div>
 <div class="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-3 gap-6 items-start">
   {#each filledVTuberGuests as guest, i (i)}
@@ -192,7 +206,7 @@
                   </svg>
                 </a>
                 {/if}
-                {#if guest.social_type === 'youtube'}
+                {#if guest.social_type === "youtube"}
                 <a
                   class="inline-block mt-3 text-red-500 hover:text-red-600"
                   href={guest.socials}
@@ -202,6 +216,19 @@
                 >
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-10 h-10" viewBox="0 0 16 15.5">
                   <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z"/>
+                </svg>
+                </a>
+                {/if}
+                {#if guest.social_type === "twitter"}
+                <a
+                  class="inline-block mt-3 text-black-500 hover:text-black-600"
+                  href={guest.socials}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  aria-label="Follow on Twitter/X"
+                >
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-10 h-10" viewBox="0 0 16 15.5">
+                  <path d="M12.6.75h2.454l-5.36 6.142L16 15.25h-4.937l-3.867-5.07-4.425 5.07H.316l5.733-6.57L0 .75h5.063l3.495 4.633L12.601.75Zm-.86 13.028h1.36L4.323 2.145H2.865z"/>
                 </svg>
                 </a>
                 {/if}
